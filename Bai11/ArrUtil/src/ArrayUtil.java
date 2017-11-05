e8a52097d7203185c346ab54e983f368710078eb
TrungNT <trungsieunhan24@gmail.com>
2017-11-05 15:35:47 +0700

    add bai10

--- Bai11/ArrUtil/src/ArrayUtil.java
+++ Bai11/ArrUtil/src/ArrayUtil.java
@@ -19,13 +19,19 @@ public class ArrayUtil {
 		}
 	}
 	public  void findTraingle(){
-		Arrays.sort(array);
+		System.out.println("Các bộ cạnh tam giác là: ");
 		for(int i=0;i<n-2;i++){
-			
-			if(array[i]+array[i+1]>array[i+2]){
-				System.out.println("ta co bo tam giac la "+array[i]+"   " +array[i+1]+"  "+array[i+2]);
+			//Giả sử: array[i] là cạnh a, array[i+1] là cạnh b, array[i+2] là cạnh c
+			//Nếu cạnh a + cạnh b > cạnh c
+			boolean ab = array[i]+array[i+1]>array[i+2];
+			//Nếu cạnh a + cạnh c > cạnh b
+			boolean ac = array[i]+array[i+2]>array[i+1];
+			//Nếu cạnh b + cạnh c > cạnh a
+			boolean bc = array[i+1]+array[i+2]>array[i];
+			if(ab && ac && bc){
+				//In ra bộ 3 cạnh a,b,c thỏa mãn là 3 cạnh tam giác
+				System.out.println(array[i]+"," +array[i+1]+","+array[i+2]);
 			}
-			
 		}
 	}
 
