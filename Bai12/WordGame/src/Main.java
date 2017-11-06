ba524baf6379b762ece1c6402f4bc16b13b1901f
TrungNT <trungsieunhan24@gmail.com>
2017-11-06 13:49:09 +0700

    add Bai12

---
+++ Bai12/WordGame/src/Main.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * Created by My PC on 06/11/2017.
+ */
+public class Main {
+	public static void main(String[] args) {
+		WordManager wordManager = new WordManager("Thể Thao");
+		wordManager.addWord("Sport","Thể Thao");
+		wordManager.addWord("Soccer","Bóng đá");
+		wordManager.addWord("Football","Bóng đá");
+		wordManager.addWord("Leg","Chân");
+		wordManager.addWord("Defender","Hậu vệ");
+		wordManager.printAllWord();
+		Word[] twoWords = wordManager.getTwoWordRandom();
+		System.out.println(Arrays.toString(twoWords));
+	}
+}
+
--- Bai12/WordGame/src/WordManager.java
+++ Bai12/WordGame/src/WordManager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by My PC on 06/11/2017.
@@ -14,7 +15,26 @@ public class WordManager {
 	public void addWord(String word, String vnMean){
 		Word wordObj = new Word(word, vnMean);
 		if(!listWord.contains(wordObj)){
-			lis
+			listWord.add(wordObj);
 		}
+	}
+	public Word[] getTwoWordRandom(){
+		Collections.shuffle(listWord);
+		Word arrWord[] = new Word[2];
+		arrWord[0] = listWord.get(0);
+		arrWord[1] = listWord.get(1);
+		return arrWord;
+	}
+	public void remove(String word){
+		for (int i=0;i<listWord.size();i++){
+			if(listWord.get(i).getWord().equals(word)){
+				listWord.remove(i);
+				return;
+			}
+		}
+	}
+	public void printAllWord(){
+		System.out.println("Category :"+category);
+		System.out.println(listWord.toString());
 	}
 }
