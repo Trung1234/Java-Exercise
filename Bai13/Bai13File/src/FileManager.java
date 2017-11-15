0ffffb208d3544ddbac7c19f0137463730f45acf
TrungNT <trungsieunhan24@gmail.com>
2017-11-15 15:59:09 +0700

    add bai13

---
+++ Bai13/Bai13File/src/FileManager.java
@@ -0,0 +1,32 @@
+import java.io.File;
+
+/**
+ * Created by My PC on 15/11/2017.
+ */
+public class FileManager {
+	private File fileSrc;
+	
+	public FileManager(File fileSrc) {
+		this.fileSrc = fileSrc;
+	}
+	
+	public FileManager(String pathSrc) {
+		this.fileSrc = new File(pathSrc);
+	}
+	public void findByExtension(String extension){
+
+		File[] listOfFiles = fileSrc.listFiles();
+		for (int i = 0; i < listOfFiles.length; i++) {
+			
+			if (listOfFiles[i].isFile()&&getFileExtension(listOfFiles[i])==extension) {
+				System.out.println("File " + listOfFiles[i].getName());
+			}
+		}
+	}
+	public String getFileExtension(File file) {
+		String fileName = file.getName();
+		if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+			return fileName.substring(fileName.lastIndexOf(".")+1);
+		else return "";
+	}
+}
